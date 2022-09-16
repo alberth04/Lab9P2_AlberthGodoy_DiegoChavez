f343400f5cacfbeca0c4110eb520a43185e25d97
removethejuice <diegojesuschavezbotto@gmail.com>
2022-09-16 13:16:11 -0600

    sjdfnkjasfb

--- src/lab9p2_alberthgodoy_diegochavez/Lab9P2_AlberthGodoy_DiegoChavez.java
+++ src/lab9p2_alberthgodoy_diegochavez/Lab9P2_AlberthGodoy_DiegoChavez.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab9p2_alberthgodoy_diegochavez;
 
-/**
- *
- * @author godoy
- */
+
 public class Lab9P2_AlberthGodoy_DiegoChavez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
